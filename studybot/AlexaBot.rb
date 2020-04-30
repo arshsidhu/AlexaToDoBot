81dae02d22aaafd01d9822212f521012155452e5
Arshdeep Sidhu <sidhu.a1998@gmail.com>
2020-04-29 23:50:34 -0400

    Added a display command

--- studybot/AlexaBot.rb
+++ studybot/AlexaBot.rb
@@ -1,7 +1,9 @@
+require 'base64'
 require 'date'
 require 'discordrb'
 require 'json'
 require 'mongo'
+require 'table_print'
 
 # Managing jsons
 envfile = File.open "environment.json"
@@ -19,7 +21,7 @@ client = Mongo::Client.new("mongodb+srv://#{env["DBUSER"]}:#{env["DBPASS"]}@clus
 
 # Globals
 TASKS = client[:Tasks]
-TAGS = ["School", "Work", "Misc"]
+TAGS = ["School", "Work", "Misc", "All"]
 
 bot = Discordrb::Commands::CommandBot.new token: env["TOKEN"], client_id: env['CLIENTID'], prefix: '!'
 
@@ -31,27 +33,81 @@ bot.command(:test, description: "Used to debug.") do |event|
     TASKS.find().each do |doc|
         event.respond JSON.pretty_generate(doc)
     end
+    return
 end
 
-# Add's tasks to the DB
+# Add's tasks to the DB.
 # Params
-# Tag = School, work or misc
-# Date = Due date of assignment
-# Description = Assignment description
+# Tag = School, work or misc.
+# Date = Due date of assignment in dd/mm/yyyy.
+# Description = Assignment description.
 bot.command(:addtask, description: "Add a task to your ToDo list.") do |event, tag, date, *description|
+
+    unless TAGS.include? tag 
+        event.respond "Improper tag"
+        return
+    end
+
+    begin
+        datetime = DateTime.strptime(date, "%d/%m/%Y")
+    rescue ArgumentError
+        event.respond "Improper date (dd/mm/YYYY)"
+        return
+    end
+
+    desc = description.join(" ")
+
+    if desc.length > 255
+        event.respond "Description too long"
+    end
+
     user = event.user.name
     num = TASKS.find({'user':user}).count
     id = TASKS.find.count
-    datetime = DateTime.strptime(date, "%d/%m/%Y")
     
     doc['_id'] = id+1
     doc['num'] = num+1
     doc['user'] = user
     doc['tag'] = tag
     doc['date'] = datetime
-    doc['description'] = description.join(" ")
+    doc['description'] = desc
 
     TASKS.insert_one(doc)
+
+    return "Insert Successful"
 end
+
+# Display the tasks based on username.
+# Params
+# Tag = ToDo task tag, leave blank to display all tasks.
+bot.command(:display, description: "Display your tasks.") do |event, tag = "All"|
+    
+    unless TAGS.include? tag
+        event.respond "Improper tag"
+        return
+    end
+
+    if tag == "All"
+        items = TASKS.find({'user':event.user.name})
+    else
+        items = TASKS.find({'user':event.user.name, 'tag':tag})    
+    end
+
+    # figure out how to properly print this later
+    # tp items, :num, :tag, :date, :description
+
+    output_str = "```- - - - - - - - - - - - - - -\n"
+    items.each do |doc|
+        output_str << task_to_s(doc) << "- - - - - - - - - - - - - - -\n"
+    end
+    output_str << "```"
+
+    return output_str
+end
+
+def task_to_s(task)
+    return "num:#{task['num']}\ntag:#{task['tag']}\ndue date:#{task['date'].to_s}\ndescription:#{task['description']}\n"
+end
+
 
 bot.run
